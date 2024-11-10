67561e424b1281d5ecc5358ceebb99f2ab7e400a
jeanrj987 <jeancarlosrj11@hotmail.com>
2024-11-10 19:39:19 -0400

    feat: ensure transaction can be created

---
+++ app/_actions/add-transactions/schema.ts
@@ -0,0 +1,13 @@
+import { TransactionCategory, TransactionPaymentMethod, TransactionType } from "@prisma/client";
+import { number, z } from "zod";
+
+export const addTransactionSchema = z.object({
+    name: z.string().trim().min(1),     
+    amount: z.number().positive(),    
+    type: z.nativeEnum(TransactionType),          
+    category: z.nativeEnum(TransactionCategory),
+    paymentMethod: z.nativeEnum(TransactionPaymentMethod), 
+    date: z.date()
+  
+}
+)
--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -32,6 +32,9 @@ import { Input } from "./ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { TRANSACTION_CATEGORY_OPTIONS, TRANSACTION_PAYMENT_METHOD_OPTIONS, TRANSACTION_TYPE_OPTIONS } from "../_constants/transaction";
 import { DatePicker } from "./ui/date-picker";
+import { addTransaction } from "../_actions/add-transactions";
+import { MoneyInput } from "./money-input";
+import { useState } from "react";
 
 
 
@@ -39,8 +42,10 @@ const formSchema = z.object({
   name: z.string().trim().min(1, {
     message: "O nome é obrigatorio.",
   }),
-  amount: z.string().trim().min(1, {
-    message: "O valor é obrigatorio.",
+  amount: z.number({
+    required_error: "O valor deve ser obrigatorio"
+  }).positive({
+    message: "O valor deve ser positivo"
   }),
   type: z.nativeEnum(TransactionType, {
     required_error: "O tipo é obrigatorio.",
@@ -59,10 +64,13 @@ const formSchema = z.object({
 type FormSchema = z.infer<typeof formSchema>;
 
 const AddTransactionButton = () => {
+
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      amount: "",
+      amount: 1,
       category: TransactionCategory.OTHER,
       date: new Date(),
       name: "",
@@ -71,12 +79,21 @@ const AddTransactionButton = () => {
     },
   });
 
-  const onSubmit = (data: FormSchema) => {
-    console.log({ data });
+  const onSubmit = async (data: FormSchema) => {
+    try {
+      await addTransaction(data)
+      setDialogIsOpen(false)
+      form.reset();
+    }catch (error){
+      console.error(error)
+    }
   };
   
   return (
-    <Dialog onOpenChange={(open) =>{
+    <Dialog
+    open={dialogIsOpen}
+    onOpenChange={(open) =>{
+      setDialogIsOpen(open)
         if (!open) {
             form.reset();
         }
@@ -117,7 +134,15 @@ const AddTransactionButton = () => {
                 <FormItem>
                   <FormLabel>Valor</FormLabel>
                   <FormControl>
-                    <Input placeholder="Digite o valor" {...field} />
+                    <MoneyInput placeholder="Digite o valor"
+                    value={field.value}
+                    onValueChange = {({floatValue})=>
+                    field.onChange(floatValue)
+                    }
+                    onBlur={field.onBlur}
+                    disabled={field.disabled} 
+                    
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
