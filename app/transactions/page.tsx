3aad3bc0046622544d53942382cd9a4e8e72dc4e
jeanrj987 <jeancarlosrj11@hotmail.com>
2024-11-15 17:56:24 -0400

    feat: ensure pages are correctly scrolled by the user

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,39 +2,36 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
-import NavBar from "../_components/navbar";
+import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
-  const {userId} = await auth()
+  const { userId } = await auth();
   if (!userId) {
-    redirect("/login")
-  };
-
+    redirect("/login");
+  }
   const transactions = await db.transaction.findMany({
     where: {
       userId,
-    }
+    },
   });
-
-    return (
-     <>
-      <NavBar /> 
-      <div className="p-6 space-y-6">
-        {/* titulo e botao */}
-        <div className=" flex w-full items-center justify-between ">
-          <h1 className="text-2xl font-bold">Transação</h1>
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-
-          <DataTable columns={transactionColumns} data={transactions}/>
-     
+        <ScrollArea>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
+    </>
+  );
+};
 
-      </>
-    )
-     
-  };
-  
-  export default TransactionsPage;
+export default TransactionsPage;
