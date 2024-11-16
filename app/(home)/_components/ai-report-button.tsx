ab22856beae7c427ad77909ae557c0a83e118432
jeanrj987 <jeancarlosrj11@hotmail.com>
2024-11-16 16:48:37 -0400

    feat: ensure only premium user can generate

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -16,13 +16,14 @@ import { generateAiReport } from "../_actions/generate-ai-report";
 import { useState } from "react";
 import { ScrollArea } from "@/app/_components/ui/scroll-area";
 import Markdown from "react-markdown";
+import Link from "next/link";
 
 interface AiReportButtonProps {
   hasPremiumPlan: boolean;
   month: string;
 }
 
-const AiReportButton = ({ month }: AiReportButtonProps) => {
+const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
   const [report, setReport] = useState<string | null>(null);
   const [reportIsLoading, setReportIsLoading] = useState(false);
 
@@ -39,11 +40,13 @@ const AiReportButton = ({ month }: AiReportButtonProps) => {
       }
     };
     return (
-      <Dialog onOpenChange={(open)=>{
-        if (!open) {
-            setReport(null)
-        }
-      }}>
+        <Dialog
+        onOpenChange={(open) => {
+          if (!open) {
+            setReport(null);
+          }
+        }}
+      >
         <DialogTrigger asChild>
           <Button variant="ghost">
             Relatório IA
@@ -51,28 +54,49 @@ const AiReportButton = ({ month }: AiReportButtonProps) => {
           </Button>
         </DialogTrigger>
         <DialogContent className="max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Relatório IA</DialogTitle>
-            <DialogDescription>
-              Use inteligência artificial para gerar um relatório com insights
-              sobre suas finanças.
-            </DialogDescription>
-          </DialogHeader>
-          <ScrollArea className="prose prose-h3:text-white prose-h4:text-white prose-strong:text-white max-h-[450px] text-white">
-            <Markdown>{report}</Markdown>
-          </ScrollArea>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="ghost">Cancelar</Button>
-            </DialogClose>
-            <Button
-              onClick={handleGenerateReportClick}
-              disabled={reportIsLoading}
-            >
-              {reportIsLoading && <Loader2Icon className="animate-spin" />}
-              Gerar relatório
-            </Button>
-          </DialogFooter>
+          {hasPremiumPlan ? (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório IA</DialogTitle>
+                <DialogDescription>
+                  Use inteligência artificial para gerar um relatório com insights
+                  sobre suas finanças.
+                </DialogDescription>
+              </DialogHeader>
+              <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+                <Markdown>{report}</Markdown>
+              </ScrollArea>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="ghost">Cancelar</Button>
+                </DialogClose>
+                <Button
+                  onClick={handleGenerateReportClick}
+                  disabled={reportIsLoading}
+                >
+                  {reportIsLoading && <Loader2Icon className="animate-spin" />}
+                  Gerar relatório
+                </Button>
+              </DialogFooter>
+            </>
+          ) : (
+            <>
+              <DialogHeader>
+                <DialogTitle>Relatório IA</DialogTitle>
+                <DialogDescription>
+                  Você precisa de um plano premium para gerar relatórios com IA.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="ghost">Cancelar</Button>
+                </DialogClose>
+                <Button asChild>
+                  <Link href="/subscription">Assinar plano premium</Link>
+                </Button>
+              </DialogFooter>
+            </>
+          )}
         </DialogContent>
       </Dialog>
     );
